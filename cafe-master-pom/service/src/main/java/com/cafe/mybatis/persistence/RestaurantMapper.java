c3b8fbd788e0629c3d7a940c154983d1770c3da7
Farzia <user@10.1.1.100>
2016-04-11 00:44:39 +0800

    Added SQL to the XML files

---
+++ cafe-master-pom/service/src/main/java/com/cafe/mybatis/persistence/RestaurantMapper.java
@@ -0,0 +1,25 @@
+package com.cafe.mybatis.persistence;
+
+import com.cafe.common.exception.BfpbException;
+import com.cafe.mybatis.domain.Division;
+import com.cafe.mybatis.domain.RestaurantData;
+
+import java.util.List;
+import java.util.Map;
+
+public interface RestaurantMapper {
+
+    void create(RestaurantData data) throws BfpbException;
+
+    void update(RestaurantData data) throws BfpbException;
+
+    List<RestaurantData> getRestaurantByParam(Map<String, Object> params) throws BfpbException;
+
+    Integer getRestaurantCountByParam(Map<String, Object> params) throws BfpbException;
+
+    RestaurantData getRestaurantByID(Integer divisionId) throws BfpbException;
+
+    Integer getDuplicateCount(Map<String, Object> params) throws BfpbException;
+    
+    Integer delete(Map<String, Object> params) throws BfpbException;
+}
---
+++ cafe-master-pom/service/src/main/java/com/cafe/service/RestaurantService.java
@@ -0,0 +1,53 @@
+package com.cafe.service;
+
+import com.cafe.common.exception.BfpbException;
+import com.cafe.mybatis.domain.Division;
+import com.cafe.mybatis.domain.RestaurantData;
+import com.cafe.mybatis.domain.UserGroupPermissionData;
+import com.cafe.mybatis.persistence.DivisionMapper;
+import com.cafe.mybatis.persistence.RestaurantMapper;
+import com.cafe.mybatis.persistence.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by farzia on 1/13/16.
+ */
+@Service
+public class RestaurantService {
+
+    @Autowired
+    private RestaurantMapper restaurantMapper;
+
+
+    public void create(RestaurantData data) throws BfpbException {
+        this.restaurantMapper.create(data);
+    }
+
+    public void update(RestaurantData data) throws BfpbException {
+        this.restaurantMapper.update(data);
+    }
+    
+    public Integer delete(Map<String, Object> params) throws BfpbException {
+    	return this.restaurantMapper.delete(params);
+    }
+
+    public List<RestaurantData> getDivisionByParam(Map<String, Object> params) throws BfpbException {
+        return this.restaurantMapper.getRestaurantByParam(params);
+    }
+
+    public Integer getRestaurantCountByParam(Map<String, Object> params) throws BfpbException {
+        return this.restaurantMapper.getRestaurantCountByParam(params);
+    }
+
+    public RestaurantData getRestaurantByID(Integer RestaurantID) throws BfpbException {
+        return this.restaurantMapper.getRestaurantByID(RestaurantID);
+    }
+
+    public Integer getDuplicateCount(Map<String, Object> params) throws BfpbException {
+        return this.restaurantMapper.getDuplicateCount(params);
+    }
+}
